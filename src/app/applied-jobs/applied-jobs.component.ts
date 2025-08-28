186f93637fb1f47fb754ea7341d0b5001193f664
Inzamamul-huq <inzamulhuq0@gmail.com>
2025-08-28 18:28:32 +0530

    aug28 5

--- src/app/applied-jobs/applied-jobs.component.ts
+++ src/app/applied-jobs/applied-jobs.component.ts
@@ -171,7 +171,7 @@ export class AppliedJobsComponent implements OnInit {
       return;
     }
 
-    this.http.get<{status: string, applications: AppliedJob[]}>(`/api/student/applications/${user.email}`)
+    this.http.get<{status: string, applications: AppliedJob[]}>(`/api/student/applications/email/${user.email}`)
       .subscribe({
         next: (response) => {
           if (response.status === 'success') {
